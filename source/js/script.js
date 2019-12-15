cd00dd1293b7a91a2ad8aea24219f7d9152e4c54
Yuriy Bevov <jury19y@mail.ru>
2019-12-16 00:36:29 +0300

    небольшие правки

--- source/js/script.js
+++ source/js/script.js
@@ -9,14 +9,59 @@ var phoneMask = IMask(
   });
 })();
 
+// Табы
+
+(function () {
+  var tab = document.querySelectorAll('.subscription__toggle-label');
+  var list = document.querySelectorAll('.subscription__list');
+
+    var onClickHandler = function () {
+    var attr = this.getAttribute('id');
+
+    for (var i = 0; i < list.length; i++) {
+      list[i].style.display = 'none';
+      list[attr].style.display = 'flex';
+    }
+  }
+
+  for (var i = 0; i < tab.length; i++) {
+    tab[i].addEventListener('click', onClickHandler);
+  }
+})();
+
 // feedback slider
 
 (function () {
-  var swiper = new Swiper('.swiper-container', {
-    //spaceBetween: 30,
+  var feedbackSwiper = new Swiper('.feedback__slider-box.swiper-container', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var coachSwiper = new Swiper('.coach__slider-box.swiper-container', {
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 0
+      },
+      767: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 30
+      },
+      1199: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+
+        spaceBetween: 40
+      }
+    },
+
+    navigation: {
+      nextEl: '.coach__btn.swiper-button-next',
+      prevEl: '.coach__btn.swiper-button-prev',
     },
   });
 })();
