32ef57ab705dfa416c3911412c3f56551479c0d1
Yuriy Bevov <jury19y@mail.ru>
2019-12-20 00:34:27 +0300

    поправил gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -106,7 +106,7 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
+    //"source/js/**",
     "source//*.ico"
     ], {
       base: "source"
